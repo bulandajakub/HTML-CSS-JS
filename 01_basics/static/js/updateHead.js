312d63f7cfd28c5e7e990107099fc2a4aa38e321
bulandajakub <bulandajakub@gmail.com>
2024-12-27 15:51:18 +0100

    Add dockstrings and comments to the code

--- 01_basics/static/js/updateHead.js
+++ 01_basics/static/js/updateHead.js
@@ -1,3 +1,10 @@
+/**
+ * Updates the title and meta description of the page
+ * @param {string} sectionName - Name of the section to be updated
+ * @param {Object} sections - Object containing information about the sections
+ * @property {string} title - The title of the page
+ * @property {string} description - The meta description of the page
+ */
 export function updateHead(sectionName, sections) {
     const sectionData = sections[sectionName];
 
@@ -8,8 +15,10 @@ export function updateHead(sectionName, sections) {
 
     const { title, description } = sectionData;
 
+    // Set the title of the page
     document.title = title || 'HTML Basics';
 
+    // Set the meta description of the page
     const metaDescription = document.querySelector('meta[name="description"]');
     if (metaDescription) {
         metaDescription.content = description || 'This page contains all the things I am learning how to create as I learn HTML.';
