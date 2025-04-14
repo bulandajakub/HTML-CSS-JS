2390935d60bda139d4016eac34569acc3bb4ca09
Jakub Bulanda <bulandajakub@gmail.com>
2025-04-14 21:10:58 +0200

    docs: add explanations for crucial CSS properties and JS functionality

--- 00-components/nav_bar/01-animated_responsive_navbar/theme.js
+++ 00-components/nav_bar/01-animated_responsive_navbar/theme.js
@@ -1,22 +1,32 @@
 const themeMap = {
   dark: "light",
   light: "solar",
-  solar: "dark"
+  solar: "dark",
 };
 
-const theme = localStorage.getItem('theme')
-  || (tmp = Object.keys(themeMap)[0],
-      localStorage.setItem('theme', tmp),
-      tmp);
+// This code checks if there is already a theme set in localStorage.
+// If not, it takes the first key from the themeMap object (which is "dark")
+// and sets it as the current theme in localStorage.
+// Then it adds the current theme as a class to the document's body element.
+const theme =
+  localStorage.getItem("theme") ||
+  ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
 const bodyClass = document.body.classList;
 bodyClass.add(theme);
 
+/*
+ * Toggles the current theme to the next theme in the sequence.
+ * Retrieves the current theme from localStorage, determines the next theme
+ * using the themeMap, updates the document's body class, and saves the new theme
+ * back to localStorage.
+ */
+
 function toggleTheme() {
-  const current = localStorage.getItem('theme');
+  const current = localStorage.getItem("theme");
   const next = themeMap[current];
 
   bodyClass.replace(current, next);
-  localStorage.setItem('theme', next);
+  localStorage.setItem("theme", next);
 }
 
-document.getElementById('themeButton').onclick = toggleTheme;
+document.getElementById("themeButton").onclick = toggleTheme;
