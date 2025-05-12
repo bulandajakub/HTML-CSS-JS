173bd8013088b0ae834825c3a9e74e3a31ed74c7
Jakub Bulanda <bulandajakub@gmail.com>
2025-05-12 19:40:13 +0200

    feat: showcase multiple ShadCN button variants and sizes on home page

--- 06-React_Next_ShadCN-UI_component_library/next-shadcn-ui-kit/app/page.tsx
+++ 06-React_Next_ShadCN-UI_component_library/next-shadcn-ui-kit/app/page.tsx
@@ -1,5 +1,19 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button>Default</Button>;
+  return (
+    <main className="h-screen flex flex-col gap-2 justify-center items-center">
+      <Button size="lg">large</Button>
+      <Button>Default</Button>
+      <Button size="sm">small</Button>
+      <Button size="icon">i</Button>
+      <Button variant="destructive">Cancel</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="secondary">2nd</Button>
+      <Button variant="ghost" size="icon" className="rounded-full">
+        🚀
+      </Button>
+    </main>
+  );
 }
