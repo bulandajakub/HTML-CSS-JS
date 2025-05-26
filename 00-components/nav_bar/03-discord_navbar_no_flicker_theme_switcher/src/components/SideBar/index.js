55022ee511954e7eac540da11eaee65af8d3f160
Jakub Bulanda <bulandajakub@gmail.com>
2025-05-26 21:02:54 +0200

    style: define Discord-like color palette in CSS variables

--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/SideBar/index.js
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/SideBar/index.js
@@ -1,6 +1,7 @@
 const SideBar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-white dark:bg-gray-900 shadow-lg">
+    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white">
+      {/* top[-20px] - tailwind will automaticly create utility classes when the value is in brackets */}
       <i>A</i>
       <i>B</i>
       <i>C</i>
