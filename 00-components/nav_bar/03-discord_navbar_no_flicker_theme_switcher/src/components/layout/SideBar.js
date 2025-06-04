9e777cebd851636247842df717a83085a0075263
Jakub Bulanda <bulandajakub@gmail.com>
2025-06-04 21:19:26 +0200

    feat(components): add reusable SideBarIcon with styled Tailwind classes and icons

--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/SideBar.js
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/SideBar.js
@@ -1,14 +1,19 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white">
       {/* top[-20px] - tailwind will automaticly create utility classes when the value is in brackets */}
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<BsPlus size="28" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="28" />} />
+      <SideBarIcon icon={<FaPoo size="28" />} />
+      <SideBarIcon icon={<BsGearFill size="28" />} />
     </div>
   );
 };
+
+const SideBarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
 
 export default SideBar;
