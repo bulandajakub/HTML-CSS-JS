089103b3e695ce6a348891a38b8a02ff5d6f3da0
Jakub Bulanda <bulandajakub@gmail.com>
2025-06-05 21:25:48 +0200

    refactor(style): extract sidebar styles to components layer and simplify JSX
    Moved sidebar and sidebar-icon utility classes into the @layer components section of globals.css.
    Simplified the JSX in SideBar.js by using class names instead of long utility chains.
    
    Also added relevant Tailwind documentation comments in globals.css for better clarity
    and future reference on how @layer and @apply should be used.

--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/SideBar.js
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/SideBar.js
@@ -3,8 +3,9 @@ import { FaFire, FaPoo } from "react-icons/fa";
 
 const SideBar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white">
-      {/* top[-20px] - tailwind will automaticly create utility classes when the value is in brackets */}
+    <div className="sidebar">
+      {/* <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white">
+      top[-20px] - tailwind will automaticly create utility classes when the value is in brackets */}
       <SideBarIcon icon={<FaFire size="28" />} />
       <SideBarIcon icon={<BsPlus size="28" />} />
       <SideBarIcon icon={<BsFillLightningFill size="28" />} />
