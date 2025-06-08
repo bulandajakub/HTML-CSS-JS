0fd461c67b847171e044e8af8eb7ba395ba1a2c0
Jakub Bulanda <bulandajakub@gmail.com>
2025-06-08 19:06:28 +0200

    feat(channelBar): initial implementation WIP

--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/app/page.tsx
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/app/page.tsx
@@ -1,9 +1,11 @@
 import SideBar from "@/components/layout/SideBar";
+import ChannelBar from "@/components/layout/ChannelBar";
 
 export default function Home() {
   return (
     <div className="flex">
       <SideBar />
+      <ChannelBar />
     </div>
   );
 }
