d3630ea25f64eb56e9bcecda7a00e9402a6eb48c
Jakub Bulanda <bulandajakub@gmail.com>
2025-06-08 12:15:17 +0200

    feat(ui): enhance sidebar with tooltips and visual dividers
    - Added `.sidebar-tooltip` and `.sidebar-hr` classes to Tailwind components layer for better modular styling
    - Implemented tooltip functionality with `group-hover` transition and default/fallback text
    - Introduced visual dividers between sidebar sections for improved UI structure

--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/SideBar.js
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/SideBar.js
@@ -6,15 +6,27 @@ const SideBar = () => {
     <div className="sidebar">
       {/* <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white">
       top[-20px] - tailwind will automaticly create utility classes when the value is in brackets */}
-      <SideBarIcon icon={<FaFire size="28" />} />
-      <SideBarIcon icon={<BsPlus size="28" />} />
-      <SideBarIcon icon={<BsFillLightningFill size="28" />} />
-      <SideBarIcon icon={<FaPoo size="28" />} />
-      <SideBarIcon icon={<BsGearFill size="28" />} />
+      <SideBarIcon icon={<FaFire size="28" />} text="hello 🐧" />
+      <Divider />
+      <SideBarIcon icon={<BsPlus size="28" />} text="more tooltips 🗿" />
+      <SideBarIcon
+        icon={<BsFillLightningFill size="28" />}
+        text="quick tooltip ⚡"
+      />
+      <SideBarIcon icon={<FaPoo size="28" />} text="no tooltip 💩" />
+      <Divider />
+      <SideBarIcon icon={<BsGearFill size="28" />} text="tooltips settings ⚙" />
     </div>
   );
 };
 
-const SideBarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
+const SideBarIcon = ({ icon, text = "tooltip 💡" }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+  </div>
+);
+
+const Divider = () => <hr className="sidebar-hr" />;
 
 export default SideBar;
