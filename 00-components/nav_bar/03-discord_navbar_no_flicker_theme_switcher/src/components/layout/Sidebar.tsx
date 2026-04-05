8f6608f49d984ed1c17821008014174dc6d033b1
kubi <bulandajakub@gmail.com>
2026-04-05 11:01:25 +0200

    feat(ui): refactor SidebarButton to match workspace Button structure
    - Refactored SidebarButton to extend React.ButtonHTMLAttributes
    - Integrated tooltip directly into SidebarButton using 'group' and 'sidebar-tooltip'
    - Restored Discord-like tooltip animations and positioning (scale-0, origin-left, left-14)
    - Simplified Sidebar.tsx usage of SidebarButton
    
    Note: Work in progress. Some features from main branch (e.g., advanced group interactions or specific icon behaviors) are still being integrated.

--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/Sidebar.tsx
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/layout/Sidebar.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
 import { FaFire, FaPoo } from "react-icons/fa";
 import SidebarButton from "@/components/ui/SidebarButton";
-import Tooltip from "@/components/ui/Tooltip";
-import tooltipStyles from "@/components/ui/Tooltip.module.css";
+
 import Divider from "@/components/ui/Divider";
 import clsx from "clsx";
 import styles from "./Sidebar.module.css";
@@ -13,53 +12,23 @@ const Sidebar: React.FC = () => {
     <div className={clsx("sidebar", styles.sidebar)}>
       {/* <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white">
       top[-20px] - tailwind will automaticly create utility classes when the value is in brackets */}
-      <div className="button-sidebar">
-        <Tooltip
-          content="hello 🐧"
-          placement="right"
-          contentClassName={`${tooltipStyles["tooltip-sidebar"]} sidebar-tooltip`}
-        >
-          <FaFire size="28" />
-        </Tooltip>
-      </div>
+      <SidebarButton tooltip="hello 🐧">
+        <FaFire size="28" />
+      </SidebarButton>
       <Divider />
-      <div className="button-sidebar">
-        <Tooltip
-          content="more tooltips 🗿"
-          placement="right"
-          contentClassName={`${tooltipStyles["tooltip-sidebar"]} sidebar-tooltip`}
-        >
-          <BsPlus size="28" />
-        </Tooltip>
-      </div>
-      <div className="button-sidebar">
-        <Tooltip
-          content="quick tooltip ⚡"
-          placement="right"
-          contentClassName={`${tooltipStyles["tooltip-sidebar"]} sidebar-tooltip`}
-        >
-          <BsFillLightningFill size="28" />
-        </Tooltip>
-      </div>
-      <div className="button-sidebar">
-        <Tooltip
-          content="no tooltip 💩"
-          placement="right"
-          contentClassName={`${tooltipStyles["tooltip-sidebar"]} sidebar-tooltip`}
-        >
-          <FaPoo size="28" />
-        </Tooltip>
-      </div>
+      <SidebarButton tooltip="more tooltips 🗿">
+        <BsPlus size="28" />
+      </SidebarButton>
+      <SidebarButton tooltip="quick tooltip ⚡">
+        <BsFillLightningFill size="28" />
+      </SidebarButton>
+      <SidebarButton tooltip="no tooltip 💩">
+        <FaPoo size="28" />
+      </SidebarButton>
       <Divider />
-      <div className="button-sidebar">
-        <Tooltip
-          content="tooltips settings ⚙"
-          placement="right"
-          contentClassName={`${tooltipStyles["tooltip-sidebar"]} sidebar-tooltip`}
-        >
-          <BsGearFill size="28" />
-        </Tooltip>
-      </div>
+      <SidebarButton tooltip="tooltips settings ⚙">
+        <BsGearFill size="28" />
+      </SidebarButton>
     </div>
   );
 };
--- 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/ui/SidebarButton.tsx
+++ 00-components/nav_bar/03-discord_navbar_no_flicker_theme_switcher/src/components/ui/SidebarButton.tsx
@@ -1,23 +1,59 @@
+import React from "react";
 import clsx from "clsx";
 import styles from "./SidebarButton.module.css";
-import Tooltip from "./Tooltip";
 
-interface SidebarButtonProps {
-  children: React.ReactNode;
-  tooltip: string;
-  className?: string;
+/**
+ * Props for the SidebarButton component.
+ *
+ * @public
+ */
+export interface SidebarButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * The content of the sidebar button (usually an icon).
+   */
+  readonly children: React.ReactNode;
+  /**
+   * The tooltip text.
+   */
+  readonly tooltip: string;
 }
 
+/**
+ * A generic, fully customizable sidebar button component.
+ */
 const SidebarButton: React.FC<SidebarButtonProps> = ({
   children,
   tooltip,
   className = "",
-}) => (
-  <Tooltip content={tooltip} placement="right">
-    <button className={clsx("button-sidebar", styles.sidebarButton, className)}>
+  disabled = false,
+  onClick,
+  ...rest
+}) => {
+  // Compose class names dynamically using clsx
+  // 'button-sidebar' for base styles and layout
+  // 'group' to handle tooltip hover state
+  // styles["button-sidebar"] for visual tokens defined in module
+  const composedClasses = clsx(
+    "button-sidebar",
+    "group",
+    styles["button-sidebar"],
+    className
+  );
+
+  return (
+    <button
+      className={composedClasses}
+      onClick={onClick}
+      disabled={disabled}
+      type="button"
+      {...rest}
+    >
       {children}
+      <span className={clsx("sidebar-tooltip", "group-hover:scale-100", styles["sidebar-tooltip"])}>
+        {tooltip}
+      </span>
     </button>
-  </Tooltip>
-);
+  );
+};
 
 export default SidebarButton;
